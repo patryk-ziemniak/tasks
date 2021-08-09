678b040788f4422c898335b48eb7c8a02ba4200b
Patryk Ziemniak <ziemniak.pat@gmail.com>
2021-08-09 14:43:30 +0200

    Initial commit

---
+++ src/main/java/com/crud/tasks/domain/TaskDto.java
@@ -0,0 +1,13 @@
+package com.crud.tasks.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class TaskDto {
+
+    private Long id;
+    private String title;
+    private String content;
+}
