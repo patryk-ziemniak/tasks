3b89983d9253ba4a409fd8d31e760f5b7453faa4
Patryk Ziemniak <ziemniak.pat@gmail.com>
2021-08-10 17:14:01 +0200

    19.2 Implemented method for accessing Task by it's Id from DB

---
+++ src/main/java/com/crud/tasks/service/DbService.java
@@ -0,0 +1,25 @@
+package com.crud.tasks.service;
+
+import com.crud.tasks.domain.Task;
+import com.crud.tasks.repository.TaskRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class DbService {
+
+    private final TaskRepository repository;
+
+    public DbService(TaskRepository repository) {
+        this.repository = repository;
+    }
+
+    public List<Task> getAllTasks() {
+        return repository.findAll();
+    }
+
+    public Task getTaskById(long taskId) {
+        return repository.findById(taskId).orElse(new Task(taskId, "No such taskId", "ERROR!"));
+    }
+}
