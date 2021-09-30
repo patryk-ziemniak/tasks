6c8d4ab0be65feb2372418d1bfab0ecd2ed64621
Patryk Ziemniak <ziemniak.pat@gmail.com>
2021-09-30 20:24:09 +0200

    Created Email Scheduler

--- src/main/java/com/crud/tasks/domain/Mail.java
+++ src/main/java/com/crud/tasks/domain/Mail.java
@@ -1,10 +1,12 @@
 package com.crud.tasks.domain;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 
 @Builder
 @Getter
+@AllArgsConstructor
 public class Mail {
 
     private final String mailTo;
